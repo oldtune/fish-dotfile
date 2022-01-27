3b168827e25d2fb123932789a075f4da0e95e689
QuackQuack <vivaladaman@gmail.com>
2022-01-27 23:38:06 +0700

    init

---
+++ config.fish
@@ -0,0 +1,30 @@
+function fish_greeting
+clear
+end
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.dotnet
+fish_add_path ~/.ghidra-sre
+fish_add_path ~/.tor-browser
+
+function desktop
+cd ~/Desktop
+end
+
+function downloads
+cd ~/Downloads
+end
+
+function edit_dns
+sudo nvim /etc/resolv.conf
+end
+
+function edit_fish_config
+nvim config.fish
+end
+
+function wireshark
+sudo wireshark
+end
---
+++ functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/fedora/.local/share/omf/themes/boxfish/fish_prompt.fish
